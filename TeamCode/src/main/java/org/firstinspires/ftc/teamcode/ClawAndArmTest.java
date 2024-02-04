a2af053b3c7d5c8006caa85f13fc0ca0aac373c2
cheesebroccoli <122124161+cheesebroccoli@users.noreply.github.com>
2024-02-04 17:09:31 -0500

    Added prelim arm code
    Just power for now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawAndArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawAndArmTest.java
@@ -2,13 +2,18 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Claw")
-public class ClawTest extends LinearOpMode {
+public class ClawAndArmTest extends LinearOpMode {
     private Servo clawLeft;
     private Servo clawRight;
     private Servo nodder;
+    private DcMotorEx extender;
+
+    private DcMotorEx rotater;
+
     double power = .005;
     /**
      * This function is executed when this Op Mode is selected from the Driver Station.
@@ -20,12 +25,8 @@ public class ClawTest extends LinearOpMode {
         clawLeft  = janx.lc;
         clawRight = janx.rc;
         nodder    = janx.nod;
-//        janx.arminit("clawLeft","clawRight","nodder","armExtension","arm rotations");
-//        leftClaw  = janx.lc;
-//        rightClaw = janx.rc;
-//        nodder    = janx.nod;
-//        extender  = janx.ext;
-//        rotater   = janx.turn;
+        extender  = janx.ext;
+        rotater   = janx.turn;
 //        clawRight = hardwareMap.get(Servo.class,"clawRight");
 //        nodder = hardwareMap.get(Servo.class,"vertical");
         waitForStart();
@@ -43,16 +44,18 @@ public class ClawTest extends LinearOpMode {
             }
         }
     }
+    /**claw- Gamepad2 left stick**/
     private void claw(double input){
-//        //the claw nodder
-//        if(gamepad2.left_stick_y>0){
-//            nodder.setPosition(nodder.getPosition()+power);
-//        }
-//        else if(gamepad2.left_stick_y<0){
-//            nodder.setPosition(nodder.getPosition()-power);
-//        }
+        //the claw nodder
+        if(gamepad2.left_stick_y>0){
+            nodder.setPosition(nodder.getPosition()+power);
+        }
+        else if(gamepad2.left_stick_y<0){
+            nodder.setPosition(nodder.getPosition()-power);
+        }
         //opens and closes claw
         telemetry.addData("input: ",input);
+        //input:gamepad2.left_stick_x, by the by
             if(input>0){
                 //Left:open
               clawLeft.setPosition(clawLeft.getPosition()-power);
@@ -82,7 +85,33 @@ public class ClawTest extends LinearOpMode {
             }
         }
     }
+    /**arm- Gamepad2 right stick**/
+    //need to add pidf!
     private void arm(){
+        //right stick y -extender
+        if(gamepad2.right_stick_y>0){
+            //goes up
+            extender.setPower(1);
+        }
+        else if(gamepad2.right_stick_y<0){
+            //goes down
+            extender.setPower(-1);
+        }
+        else{
+            extender.setPower(0);
+        }
+        //Right stick x- turn
+        if(gamepad2.right_stick_x<0){
+            //goes...left?
+            rotater.setPower(-1);
+        }
+        else if(gamepad2.right_stick_x>0){
+            //goes... right?
+            rotater.setPower(1);
+        }
+        else{
+            rotater.setPower(0);
+        }
 
     }
 }
